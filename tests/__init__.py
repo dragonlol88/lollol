5ed021d0938fb5901d8741796c920262e8d4bc2b
sunny4249 <uram24@42maru.com>
2021-10-15 14:39:45 +0900

    clean code

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,7 @@
-from src.lollol import PermissionManager, LoginManager
-from src.lollol import lookup_permission_obj
-
+from lollol import PermissionManager
+from lollol import LoginManager
+from lollol import lookup_permission_obj
+from lollol import authorize_required
+from lollol import authorize_router
+from lollol import authorize_app
+from lollol._exceptions import ScopeNotSpecified
