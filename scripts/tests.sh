bba5f54f220847147b949d3f050eea94bea5c967
sunny4249 <uram24@42maru.com>
2021-10-15 13:52:51 +0900

    add test script

---
+++ scripts/tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+test_dir="tests"
+
+test_files=($(ls tests))
+
+for file_name in "${test_files[@]}"; do
+  file="$test_dir/$file_name"
+  if [ -f $file ] && [ $file_name != "__init__.py" ]; then
+    pytest  -W ignore::DeprecationWarning  $file
+  fi
+done
+
+
