09fa21d220f2b385bd7effc58e68031d90141869
sunny4249 <uram24@42maru.com>
2021-10-16 11:47:21 +0900

    initial commit

---
+++ example/auth_per_app.py
@@ -0,0 +1,55 @@
+import lollol
+
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.security import SecurityScopes
+
+
+# secret key to decipher and encipher the token
+secret_key = "test_secret"
+token_url  = '/auth'
+
+# Login manager initialize
+manager = lollol.LoginManager(secret_key, token_url, use_header=True)
+manager.app_name = "example_app"
+
+# Permission manager initialize
+lollol.PermissionManager(manager)
+app = FastAPI()
+
+# Authorization per router
+lollol.authorize_app(app, SecurityScopes(["users"]))
+
+
+class Users(BaseModel):
+    id: int
+    name: str
+    passwd: str
+    email: str
+
+
+users = []
+
+
+def get_fake_user(user_id):
+    for user in users:
+        if user["user_id"] == user_id:
+            return Users(users)
+    return
+
+
+def create_fake_user(user: Users):
+    users.append(user.dict())
+    return user
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: str):
+    user = get_fake_user(user_id)
+    return user
+
+
+@app.post("/users")
+async def create_user(user: Users):
+    user = create_fake_user(user)
+    return user
---
+++ example/auth_per_function.py
@@ -0,0 +1,56 @@
+import lollol
+
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.security import SecurityScopes
+
+"""
+If you 
+"""
+# secret key to decipher and encipher the token
+secret_key = "test_secret"
+token_url  = '/auth'
+
+# Login manager initialize
+manager = lollol.LoginManager(secret_key, token_url, use_header=True)
+manager.app_name = "example_app"
+
+# Permission manager initialize
+perm = lollol.PermissionManager(manager)
+
+app = FastAPI()
+
+
+class Users(BaseModel):
+    id: int
+    name: str
+    passwd: str
+    email: str
+
+users = []
+
+
+def get_fake_user(user_id):
+    for user in users:
+        if user["user_id"] == user_id:
+            return Users(users)
+    return
+
+
+def create_fake_user(user: Users):
+    users.append(user.dict())
+    return user
+
+
+@app.get("/users/{user_id}")
+@lollol.authorize_required
+async def get_user(user_id: str, scopes=SecurityScopes(["user", "user:read"])):
+    user = get_fake_user(user_id)
+    return user
+
+
+@app.post("/users")
+@lollol.authorize_required
+async def create_user(user: Users, scopes=SecurityScopes(["user", "user:create"])):
+    user = create_fake_user(user)
+    return user
